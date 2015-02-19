8d53662c0ba2ff25abd5915e5d0f53a356b960a8
spboardman <stuart@hexsys.co.uk>
2015-02-19 15:56:49 +0000

    Create weatherupdate.php

---
+++ weatherupdate.php
@@ -0,0 +1,76 @@
+<?php
+// Script to update local version of weather.xml from Met Office's Datapoint API Service
+// PLEASE NOTE: Lines 6 and 9 require editing before using this script
+
+// Remote file to download
+$remoteFile = 'http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/xml/YOURLOCATIONCODEHERE?res=3hourly&key=YOURDATAPOINTAPIKEYHERE';
+
+// Local file to write xml data to (weather.xml). For Cpanel users it will usually be /home/USERNAME/public_html/weather.xml
+$localfile = 'RELATIVEURLHERE/weather.xml';
+
+//
+
+//	NO NEED TO EDIT BELOW CODE
+
+//
+
+// Connection time out
+$connTimeout = 10;
+$url = parse_url($remoteFile);
+$host = $url['host'];
+$path = isset($url['path']) ? $url['path'] : '/';
+
+if (isset($url['query'])) {
+  $path .= '?' . $url['query'];
+}
+
+$port = isset($url['port']) ? $url['port'] : '80';
+$fp = @fsockopen($host, '80', $errno, $errstr, $connTimeout );
+if(!$fp){
+  // On connection failure return the cached file (if it exist)
+  if(file_exists($localfile)){
+    readfile($localfile);
+  }
+} else {
+  // Send the header information
+  $header = "GET $path HTTP/1.0\r\n";
+  $header .= "Host: $host\r\n";
+  $header .= "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6\r\n";
+  $header .= "Accept: */*\r\n";
+  $header .= "Accept-Language: en-us,en;q=0.5\r\n";
+  $header .= "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7\r\n";
+  $header .= "Keep-Alive: 300\r\n";
+  $header .= "Connection: keep-alive\r\n";
+  $header .= "Referer: http://$host\r\n\r\n";
+  fputs($fp, $header);
+  $response = '';
+
+  // Get the response from the remote server
+  while($line = fread($fp, 4096)){
+    $response .= $line;
+  }
+
+  // Close the connection
+  fclose( $fp );
+
+  // Remove the headers
+  $pos = strpos($response, "\r\n\r\n");
+  $response = substr($response, $pos + 4);
+
+  // Return the processed response
+  echo $response;
+
+  // Save the response to the local file
+  if(!file_exists($localfile)){
+    // Try to create the file, if doesn't exist
+    fopen($localfile, 'w');
+  }
+
+  if(is_writable($localfile)) {
+    if($fp = fopen($localfile, 'w')){
+      fwrite($fp, $response);
+      fclose($fp);
+    }
+  }
+}
+?>
